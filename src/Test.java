f6049b031c9e4c90ad3303994f05ae99f0725bf0
simpleBug <63007346+simpleBug@users.noreply.github.com>
2020-04-02 23:26:20 +0800

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,7 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("呵呵大");
-        System.out.println("呵呵大大大大");
-        System.out.println("呵呵大大大大");
+        System.out.println("呵呵大1");
+        System.out.println("呵呵大大大大1");
+        System.out.println("呵呵大大大大1");
     }
 }
